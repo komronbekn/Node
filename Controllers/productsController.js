94836bda772c813fe2db917f9054f0f32e61961b
komronbekn <nurdullaevkomron0@gmail.com>
2024-10-10 14:30:53 +0500

    get products by id

--- Controllers/productsController.js
+++ Controllers/productsController.js
@@ -28,5 +28,20 @@ const getProducts = async (req, res) => {
     }
 };
 
+const deleteProduct = async (req, res) => {
+    const { id } = req.params;
 
-module.exports = { createProduct, getProducts };
+    try {
+        const product = await Product.findByIdAndDelete(id);
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+        res.json({ message: `Product with id ${id} deleted successfully` });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server Error' });
+    }
+};
+
+
+module.exports = { createProduct, getProducts, deleteProduct };
--- Routes/products.js
+++ Routes/products.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { createProduct, getProducts } = require('../Controllers/productsController');
+const { createProduct, getProducts, deleteProduct } = require('../Controllers/productsController');
 
 
 router.post('/', createProduct);
 router.get('/', getProducts);
-
+router.get('/:id', getProducts);
+router.delete('/', deleteProduct)
 
 module.exports = router;
